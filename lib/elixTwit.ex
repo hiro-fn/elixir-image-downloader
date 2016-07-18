1748ab0183fbc8de1eb69c42e40f410a079db499
hiro-fn <hiro.fdsq@gmail.com>
2016-07-18 22:04:51 +0900

    save imgfile name change

--- lib/elixTwit.ex
+++ lib/elixTwit.ex
@@ -1,12 +1,14 @@
 defmodule GetImage do
-  def get_image(url) do
+  def get_image(param) do
     HTTPoison.start
 
-    file_name = hd(Enum.reverse(String.split(url, "/")))
+    user_name = param[:name]
+    url = param[:url]
+    file_name = hd(Enum.reverse(String.split(param[:url], "/")))
 
     IO.puts "Start Download #{url}"
     body = HTTPoison.get!(url).body
-    File.write!("./img/#{file_name}", body)
+    File.write!("./img/#{user_name}@#{file_name}", body)
     IO.puts "Done! #{url}"
   end
 end
@@ -18,7 +20,7 @@ defmodule ElixTwit do
       Stream.filter(fn(x) -> is_nil(x.retweeted_status) end) |>
       Stream.filter(fn(x) -> Map.has_key?(x.entities, :media) end) |>
       Stream.filter(fn(x) -> x.lang === "ja" end) |>
-      Stream.map(fn(x) -> hd(x.entities.media).media_url end) |>
+      Stream.map(fn(x) -> %{name: x.user.screen_name, url: hd(x.entities.media).media_url} end) |>
       Stream.map(fn(x) -> GetImage.get_image(x) end)
     Enum.to_list(stream)
   end
